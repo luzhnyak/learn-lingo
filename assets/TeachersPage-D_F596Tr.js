a58c6e3114ba1c89601349dc51109387cd1eab54
luzhnyak <oleg.luzhnyak@gmail.com>
2024-03-28 23:07:59 +0000

    Deploying to gh-pages from @ luzhnyak/learn-lingo@fa19b34d0b19fc1789302c0797aaf39ac46bea50 🚀

---
+++ assets/TeachersPage-D_F596Tr.js
@@ -0,0 +1,1 @@
+import{u as o,r as c,j as e}from"./index-B_4zbUOr.js";import{C as i}from"./Card-CAxJYwWe.js";const n=()=>{const{items:s,loadTeachers:a}=o(r=>({loading:r.loading,error:r.error,items:r.items,loadTeachers:r.loadTeachers}));return c.useEffect(()=>{a()},[a]),e.jsx("main",{className:"container",children:s.map(r=>e.jsx(i,{data:r},r.id))})};export{n as default};
